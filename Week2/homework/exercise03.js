742994fb95668953c9e538207fcd7ba55093895b
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-11-12 08:58:11 +0100

    'workng on week2 javascript1 homework'

---
+++ Week2/homework/exercise03.js
@@ -0,0 +1,11 @@
+let myMealRecipe = [
+  'Title: "My everyday breakfast"',
+  'Servings: 1',
+  'Ingredients: ["200ml milk", "100ml oats", "some barries"]'
+];
+
+for (let i = 0; i < 3; i++) {
+
+
+  console.log(myMealRecipe[i]);
+}
---
+++ Week2/homework/exercise04.js
@@ -0,0 +1,18 @@
+let readingList = [
+  {
+    Title: "Harry Potter",
+    Autor: "J. K. Rolling",
+    alreadyRead: true
+  },
+  {
+    Title: "The Lord Of The Rings",
+    Autor: "J. R. R. Tolkien",
+    alreadyRead: false
+  },
+  {
+    Title: "The Alchemist",
+    Autor: "Paulo Coelho",
+    alreadyRead: true
+  }
+]
+console.log(readingList[0])
