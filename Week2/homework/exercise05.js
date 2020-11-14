73568c0ad83d27101c309ee4ab2eb85ea20b3a68
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-11-14 21:13:29 +0100

    exercise05 still needs work

--- Week2/homework/exercise05.js
+++ Week2/homework/exercise05.js
@@ -1,9 +1,6 @@
-
-
 const drinkTypes = ['cola', 'lemonade', 'water'];
 const drinkTray = [];
 for (let i = 0; i < 5; i++) {
   drinkTray.push(drinkTypes[Math.floor(Math.random() * 3)])
 }
 console.log("Hey guys, I brought a " + drinkTray[0] + "," + drinkTray[1] + "," + drinkTray[2] + "," + drinkTray[3] + "," + drinkTray[4] + "!")
-
