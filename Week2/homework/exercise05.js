181cea1d735b5dfd22cb433fd632f72a9aa6e49a
Nizami Mursudlu <nizamimursudlu@gmail.com>
2020-11-14 12:10:17 +0100

    exercise05 still needs some work

--- Week2/homework/exercise05.js
+++ Week2/homework/exercise05.js
@@ -1,6 +1,9 @@
 
 
-function multyply(numberA, numberB) {
-  return numberA * numberB;
+const drinkTypes = ['cola', 'lemonade', 'water'];
+const drinkTray = [];
+for (let i = 0; i < 5; i++) {
+  drinkTray.push(drinkTypes[Math.floor(Math.random() * 3)])
 }
-multyply(3, 4); // ?
+console.log("Hey guys, I brought a " + drinkTray[0] + "," + drinkTray[1] + "," + drinkTray[2] + "," + drinkTray[3] + "," + drinkTray[4] + "!")
+
